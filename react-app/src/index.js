8fc8ecd7458b0f7652ab2201892c913c151cc4f6
AswathyVNair <aswathy.code@gmail.com>
2023-08-07 15:44:42 +0530

    added array of objects and used map function to wrap each element

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,54 +4,42 @@ import ReactDOM from "react-dom";
 //CSS
 import "./index.css";
 //setup vars
-const firstBook = {
-  title: "I Love You to the Moon and Back",
-  author: "Amelia Hepworth",
-  img: "https://static.tvtropes.org/pmwiki/pub/images/81eb7ckul.jpg",
-};
-
-const secondBook = {
-  title: "Our Class is a Family",
-  author: "Shannon Olsen",
-  img: "https://m.media-amazon.com/images/I/91NBDxB0JyL._AC_UY436_FMwebp_QL65_.jpg",
-};
+const books = [
+  {
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+    img: "https://m.media-amazon.com/images/I/91hIsjFuICL._AC_UY436_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "Our Class is a Family",
+    author: "Shannon Olsen",
+    img: "https://m.media-amazon.com/images/I/91NBDxB0JyL._AC_UY436_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "The Vanishing Half",
+    author: "Brit Bennett ",
+    img: "https://m.media-amazon.com/images/I/71AhaMqMsxL._AC_UY436_FMwebp_QL65_.jpg",
+  },
+];
 
 function BookList() {
   return (
     <section className="bookList">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        const { title, author, img } = book;
+        return <Book book={book} />;
+      })}
     </section>
   );
 }
 
-const Book = ({ img, title, author, children }) => {
+const Book = (props) => {
+  const { title, author, img } = props.book;
   return (
     <article className="book">
       <img src={img} alt="Coverpage of the book"></img>
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
