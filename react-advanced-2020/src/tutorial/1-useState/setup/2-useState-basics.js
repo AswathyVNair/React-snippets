cba4bf956f639f81b042aa6e3fa0cf4cc8fbb1ae
AswathyVNair <aswathy.code@gmail.com>
2023-08-17 19:47:31 +0530

    added useState hook

--- react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("Random Title");
+
+  const handleClick = () => {
+    if (text == "Random Title") {
+      setText("Hello World");
+    } else {
+      setText("Random Title");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className="btn" onClick={handleClick}>
+        changeTitle
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
